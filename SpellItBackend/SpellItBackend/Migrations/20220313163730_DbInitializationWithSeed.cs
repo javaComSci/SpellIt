15abf2c0d5aab35bc70be92379d8366f162b52f2
javaComSci <iramanat@purdue.edu>
2022-03-13 12:39:54 -0400

    data migration

---
+++ SpellItBackend/SpellItBackend/Migrations/20220313163730_DbInitializationWithSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpellItBackend.Migrations
+{
+    public partial class DbInitializationWithSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WordLists",
+                columns: table => new
+                {
+                    WordListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WordListName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwningUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WordLists", x => x.WordListId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    WordId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WordListId = table.Column<int>(type: "int", nullable: false),
+                    WordName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.WordId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "WordLists",
+                columns: new[] { "WordListId", "OwningUserId", "WordListName" },
+                values: new object[] { 1, 1, "vegetables" });
+
+            migrationBuilder.InsertData(
+                table: "Words",
+                columns: new[] { "WordId", "WordListId", "WordName" },
+                values: new object[] { 1, 1, "avacado" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WordLists");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
